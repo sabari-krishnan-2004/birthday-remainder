bcfc6e38edaca5cf365378601c84fa119953651d
unknown <psabarikrishnan@gmail.com>
2025-10-04 19:38:12 +0530

    phrase 2 done

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import birthdayRoutes from "./routes/birthday.routes.js";
+import cron from 'node-cron';
 
-// ✅ Load environment variables FIRST!
+
 dotenv.config();
 
-// Now import other modules that use those variables
-import connectDB from "./config/database.js";
 
-// Connect to the database
+import connectDB from "./config/database.js";
+import { checkBirthdays } from "./services/reminder.service.js";
+
 connectDB();
 
 const app = express();
 
-// A simple route to check if the server is running
+
 app.get("/", (req, res) => {
   res.send("Server is running and ready to connect!");
 });
@@ -24,6 +25,16 @@ app.use(express.json());
 
 app.use("/api/birthdays", birthdayRoutes);
 
+cron.schedule(
+  "0 0 * * *", // This means at minute 0 of hour 0 (midnight)
+  () => {
+    checkBirthdays();
+  },
+  {
+    timezone: "Asia/Kolkata", // Specify the IST timezone
+  }
+);
+
 app.listen(PORT, () => {
-  console.log(`🚀 Server is listening on port ${PORT}`);
+  console.log(` Server is listening on port ${PORT}`);
 });
---
+++ services/reminder.service.js
@@ -0,0 +1,27 @@
+import Birthday from "../models/birthday.model.js";
+
+export const checkBirthdays = async() => {
+    console.log("Checking for today's birthdays...");
+    const today = new Date();
+    const tomorrow = new Date();
+    tomorrow.setDate(today.getDate() + 1);
+
+    try{
+        const birthdays = await Birthday.find({});
+
+        for(const person of birthdays){
+            const dob = new Date(person.dob);
+            if(dob.getDate() === today.getDate() && dob.getMonth() === today.getMonth()){
+                console.log(`Today is ${person.name}'s birthday!`);
+            }
+            if(dob.getDate() === tomorrow.getDate() && dob.getMonth() === tomorrow.getMonth()){
+                console.log(`Tomorrow is ${person.name}'s birthday!`);
+            }
+            
+        }
+    }catch(err){
+        console.error("Error checking birthdays:", err);
+    }
+        
+
+}
